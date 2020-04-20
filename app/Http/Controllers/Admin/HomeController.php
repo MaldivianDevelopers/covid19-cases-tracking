639a68433b647d4cfb2f95468a2e5c6146e7eb3d
Raf <7909000@gmail.com>
2020-04-20 15:40:11 +0500

    fix date format to UTC

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -18,7 +18,7 @@ class HomeController
             'aggregate_function'    => 'count',
             'filter_field'          => 'created_at',
             'filter_days'           => '90',
-            'group_by_field_format' => 'd-m-Y',
+            'group_by_field_format' => 'Y-m-d',
             'column_class'          => 'col-md-12',
             'entries_number'        => '5',
         ];

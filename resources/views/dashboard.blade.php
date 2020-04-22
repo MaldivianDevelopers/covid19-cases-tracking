3cfebfad19d144b360a5115a6484f75f356302a9
Raf <7909000@gmail.com>
2020-04-22 14:59:42 +0500

    merge dev
    commit c114f9f76d21f1d8eff7aeed861811df68104253
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 14:59:16 2020 +0500
    
        change title on dashboard
    
    commit e21b66e2a1bf38e1ac1656320fe916d0165a5636
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 14:53:15 2020 +0500
    
        fix responsiveness
    
    commit 99a666f7106bfd6999bdcf76bfbf5a7b0de251e2
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 10:28:18 2020 +0500
    
        wip
    
    commit 0a7c60cf773c0e0591c8d8055c372d22db4ffb22
    Merge: 9f44a9a e6b764d
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 01:12:59 2020 +0500
    
        Merge branch 'master' into develop
    
        # Conflicts:
        #	app/Http/Controllers/HomeController.php
    
    commit 9f44a9aaf81dd6382fa92f0600f639b4d2de22c0
    Merge: 0cd1164 1e542f8
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 00:44:44 2020 +0500
    
        Merge branch 'feature/frontend' into develop
    
    commit 1e542f8992687a760187c91e91a98dad83ba56a1
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 00:44:33 2020 +0500
    
        frontend
    
    commit f46b7ca43f5c3ec5f34157640f3d3907f1439f5c
    Merge: 55e3c96 0cd1164
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 00:03:35 2020 +0500
    
        Merge branch 'develop' into feature/frontend
    
    commit 0cd11643ac9f8742d351dfe1e8cf35ad0381f0ef
    Merge: 8018a22 7e1e82a
    Author: Raf <7909000@gmail.com>
    Date:   Wed Apr 22 00:00:05 2020 +0500
    
        Merge branch 'master' into develop
    
        # Conflicts:
        #	app/Http/Requests/StoreCovidCaseRequest.php
        #	resources/lang/en/cruds.php
        #	resources/views/admin/covidCases/create.blade.php
    
    commit 55e3c96c1fe3814c2ab2ea1555b947bf3b081200
    Author: Raf <7909000@gmail.com>
    Date:   Tue Apr 21 22:47:55 2020 +0500
    
        wip
    
    commit 8018a2201bd7dab4444e5c8d94de899d6362d4cf
    Author: Raf <7909000@gmail.com>
    Date:   Tue Apr 21 15:11:13 2020 +0500
    
        add bulk entry

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,7 +61,7 @@
         <div class="lg:text-center">
             <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">COVID19 CASE REPORTING</p>
             <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-                TRACKING OF MALDIVES COVID19 CASES
+                TRACKING FOR MALDIVES
             </h3>
             <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
                 This is an initiative taken by developers of Maldives. A collaborative effort to keep track of cases with support from the public communities.
